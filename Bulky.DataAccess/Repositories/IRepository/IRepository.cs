af7204842163486c9bb9f96eb93c64fbb39df030
nthuy <thuynguyen.webdev@gmail.com>
2023-06-12 21:43:56 +0700

    create IRepository

---
+++ Bulky.DataAccess/Repositories/IRepository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+
+namespace Bulky.DataAccess.Repositories.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+
+        T Get(Expression<Func<T, bool>> filter);
+
+        void Add(T entity);
+
+        void Delete(T entity);
+
+        void Remove(T entity);
+
+        void RemoveRange(IEnumerable<T> entities);
+    }   
+}
---
+++ Bulky.DataAccess/Repositories/Repository.cs
@@ -0,0 +1,50 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repositories.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.DataAccess.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext db) {
+            _db = db;
+            dbSet = _db.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(System.Linq.Expressions.Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+    }
+}
