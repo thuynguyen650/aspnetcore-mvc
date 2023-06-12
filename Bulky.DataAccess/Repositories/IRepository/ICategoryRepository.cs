10fefa565847b3276d03cf0671a7ec9ccc46e392
nthuy <thuynguyen.webdev@gmail.com>
2023-06-12 21:44:20 +0700

    create CategoryRepository

---
+++ Bulky.DataAccess/Repositories/IRepository/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using Bulky.Models;
+
+namespace Bulky.DataAccess.Repositories.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category obj);
+        void Save();
+    }
+}
